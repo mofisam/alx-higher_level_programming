46662e57e153b29b110dd9c20115ca77c3c6a5af
Samuel <malaosamuel2020@gmail.com>
2024-01-09 01:58:17 +0000

    Python - Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+'''module for task 4'''
 
 
 def inherits_from(obj, a_class):
-    if type(obj) is not a_class and isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    '''
+    True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False
+    '''
+    return type(obj) != a_class and issubclass(type(obj), a_class)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
+""" module for Geometry """
 
 
-class BaseGeometry:
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ module for rectangle class """
+
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ Initialize rectangle object """
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
         self.__width = width
         self.__height = height
 
-    def __str__(self):
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-
     def area(self):
-        return self.__height * self.__width
+        """ returns the area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
