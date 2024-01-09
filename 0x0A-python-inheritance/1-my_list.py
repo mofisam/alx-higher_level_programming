4440263c3e2e9fae0918d42bad08a571c46d141c
Samuel <malaosamuel2020@gmail.com>
2024-01-09 01:29:22 +0000

    Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" module for class list and its sub class"""
+"""module for class list and its sub class"""
 
 class MyList(list):
-	""" subclass inherites from list class """
+	"""subclass inherites from list class"""
 
 	def print_sorted(self):
-	 """ print a sorted copy of list"""
+	 """print a sorted copy of list"""
 	new_list = list.copy(self)
 	list.sort(new_list)
 	print(new_list)
