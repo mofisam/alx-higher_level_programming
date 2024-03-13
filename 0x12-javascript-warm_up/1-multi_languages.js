e05ec3e35a2997a0bcf09a3da7f2b1512a9cf1fa
Samuel <malaosamuel2020@gmail.com>
2024-03-13 23:49:02 +0000

    javascript-warm_up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
+  elem => console.log(elem)
+);
