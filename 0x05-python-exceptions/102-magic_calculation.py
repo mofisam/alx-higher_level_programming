e1219e7aaf1cc7698e862e15c06a709949a589cf
Samuel <malaosamuel2020@gmail.com>
2024-05-23 01:23:56 +0000

    python-exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,19 +1,14 @@
-c_calculation(a, b):
-    """function that does exactly the same as the given Python bytecode.
-
-    Args:
-        a: integer
-        b: integer
-    """
+#!/usr/bin/python3
+def magic_calculation(a, b):
     result = 0
-
-    for num in range(1, 3):
+    for i in range(1, 3):
         try:
-            if num > a:
-                raise Exception("Too far")
+            if (i > a):
+                raise (Exception('Too far'))
             else:
-                result += (a ** b) / num
-        except Exception:
-            result = b + a
+
+                result = (((a ** b) / i) + result)
+        except (Exception):
+            result = (b + a)
             break
-    return result
+    return (result)
